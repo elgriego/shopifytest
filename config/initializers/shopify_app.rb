af6b8e2a08e70718a7ef1d3b063061b0e5815bff
elgriego <agustinqm@gmail.com>
2020-05-09 13:50:18 -0300

    added the shopify gem

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,12 @@
+ShopifyApp.configure do |config|
+  config.application_name = "My Shopify App"
+  config.api_key = "d65cb26014299b6927b13a8a9855ace7"
+  config.secret = "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"
+  config.old_secret = "<old_secret>"
+  config.scope = "read_products" # Consult this page for more scope options:
+                                 # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
+  config.embedded_app = true
+  config.after_authenticate_job = false
+  config.api_version = ""
+  config.session_repository = Shop
+end
