46dc859a236d86569d767c12b0a57d718e890ebd
elgriego <agustinqm@gmail.com>
2020-05-13 14:19:47 -0300

    added key

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = "986b091bccfe7954e103a0e7f63c22ed"
-  config.secret = "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"
+  config.api_key = "571f70b5ac1425c8499188a61e7281ba"
+  config.secret = "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"
   config.old_secret = "<old_secret>"
   config.scope = "read_products" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
