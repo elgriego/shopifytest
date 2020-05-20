748f93589b1c2a00cd969899398b111a3f389a7f
elgriego <agustinqm@gmail.com>
2020-05-20 17:18:57 -0300

    a

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -30,7 +30,7 @@ class HomeController < AuthenticatedController
 		creds = {
       		client_id: "4777ABAC5CEA47D38468FACC71D30A7F",
       		client_secret: "CyIkjr1DA8hT6qDHPXU60RKeli8qFh57RXktgRDkhPF6eKVI",
-      		redirect_uri: "https://teststore20203.myshopify.com/callback",
+      		redirect_uri: "https://teststore20203.myshopify.com/admin/apps/test-app-1986",
       		scopes: "openid profile email accounting.settings accounting.reports.read accounting.journals.read accounting.contacts accounting.attachments accounting.transactions assets assets.read projects projects.read offline_access"
     	}
     	
