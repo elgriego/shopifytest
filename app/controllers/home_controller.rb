a54120fea25df336277519654f350fd7a12b8afa
elgriego <agustinqm@gmail.com>
2020-05-21 10:53:42 -0300

    asd

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class HomeController < AuthenticatedController
-
 	require 'xero-ruby'
 
 	def index
@@ -44,6 +43,16 @@ class HomeController < AuthenticatedController
 	end
 
 	def callback
+		creds = {
+      		client_id: "4777ABAC5CEA47D38468FACC71D30A7F",
+      		client_secret: "CyIkjr1DA8hT6qDHPXU60RKeli8qFh57RXktgRDkhPF6eKVI",
+      		redirect_uri: "https://shopifygt.herokuapp.com/callback",
+      		scopes: "openid profile email accounting.settings accounting.reports.read accounting.journals.read accounting.contacts accounting.attachments accounting.transactions assets assets.read projects projects.read offline_access"
+    	}
+    	
+    	@xero_client ||= XeroRuby::ApiClient.new(credentials: creds)
+
+	    return @xero_client
 	    @token_set = @xero_client.get_token_set_from_callback(params)
 	    # you can use `@xero_client.connections` to fetch info about which orgs
 	    # the user has authorized and the most recently connected tenant_id
